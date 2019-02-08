4fbbd30cc5aa11de1f9a3f861dd36670742e07e7
k-hiruma <k-hiruma@colopl.co.jp>
2019-02-08 18:59:18 +0900

    First commit.

---
+++ Assets/SelectGUICaptureSample/Scripts/SelectGUICapture.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SelectGUICapture : MonoBehaviour
+{
+    public event System.Action<RenderTexture> OnCaptured;
+
+    [SerializeField, Tooltip("GUIをレンダリングしているカメラ")]
+    private Camera _guiCamera = null;
+
+    [SerializeField, Tooltip("キャプチャするタイミング")]
+    private CameraEvent _cameraEvent = CameraEvent.BeforeImageEffects;
+
+    [SerializeField, Tooltip("合成時に無視されるUIのレイヤー")]
+    private LayerMask _captureTargetLayer = -1;
+
+    private Camera _mainCamera = null;
+    private RenderTexture _buf = null;
+    private CommandBuffer _commandBuffer = null;
+
+    #region ### MonoBehaviour ###
+    private void Awake()
+    {
+        CreateBuffer();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            TakeScreenshot();
+        }
+    }
+
+    /// <summary>
+    /// 動作確認用にGizmoでテクスチャを表示する
+    /// </summary>
+    private void OnGUI()
+    {
+        if (_buf == null) return;
+        GUI.DrawTexture(new Rect(5f, 5f, Screen.width * 0.5f, Screen.height * 0.5f), _buf);
+    }
+    #endregion ### MonoBehaviour ###
+
+    /// <summary>
+    /// バッファを生成する
+    /// </summary>
+    private void CreateBuffer()
+    {
+        _buf = new RenderTexture(Screen.width, Screen.height, 0);
+
+        _commandBuffer = new CommandBuffer();
+        _commandBuffer.name = "CaptureScene";
+        _commandBuffer.Blit(BuiltinRenderTextureType.CurrentActive, _buf);
+    }
+
+    /// <summary>
+    /// スクリーンショットを撮影する
+    /// </summary>
+    public void TakeScreenshot()
+    {
+        AddCommandBuffer();
+
+        StartCoroutine(WaitCapture());
+    }
+
+    /// <summary>
+    /// コマンドバッファの処理を待つ
+    /// </summary>
+    private IEnumerator WaitCapture()
+    {
+        yield return new WaitForEndOfFrame();
+
+        BlendGUI();
+
+        if (OnCaptured != null)
+        {
+            OnCaptured.Invoke(_buf);
+        }
+
+        RemoveCommandBuffer();
+    }
+
+    /// <summary>
+    /// GUI要素をブレンドする
+    /// </summary>
+    private void BlendGUI()
+    {
+        _guiCamera.targetTexture = _buf;
+
+        int tmp = _guiCamera.cullingMask;
+        _guiCamera.cullingMask = _captureTargetLayer;
+
+        _guiCamera.Render();
+
+        _guiCamera.cullingMask = tmp;
+
+        _guiCamera.targetTexture = null;
+    }
+
+    /// <summary>
+    /// メインカメラにコマンドバッファを追加する
+    /// </summary>
+    private void AddCommandBuffer()
+    {
+        if (_mainCamera == null)
+        {
+            _mainCamera = Camera.main;
+        }
+
+        _mainCamera.AddCommandBuffer(_cameraEvent, _commandBuffer);
+    }
+
+    /// <summary>
+    /// メインカメラからコマンドバッファを削除する
+    /// </summary>
+    private void RemoveCommandBuffer()
+    {
+        if (_mainCamera == null)
+        {
+            return;
+        }
+
+        _mainCamera.RemoveCommandBuffer(_cameraEvent, _commandBuffer);
+    }
+} 
---
+++ Assets/SelectGUICaptureSample/Scripts/StoreScreenshot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class StoreScreenshot : MonoBehaviour
+{
+    [SerializeField]
+    private SelectGUICapture _capture = null;
+
+    private void Awake()
+    {
+        _capture.OnCaptured += Store;
+    }
+
+    private void Store(RenderTexture buffer)
+    {
+        // 対象バッファのサイズと同じTexture2Dを生成する
+
+        int width = buffer.width;
+        int height = buffer.height;
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+        // 現在のアクティブなRenderTextureをバックアップしておく
+        RenderTexture tmp = RenderTexture.active;
+
+        // アクティブなバッファを引数のバッファに差し替える
+        RenderTexture.active = buffer;
+
+        // アクティブなバッファからピクセルを読み込む
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+
+        // バックアップしていたものに戻す
+        RenderTexture.active = tmp;
+
+        // 読み込んだピクセルをPNGとしてエンコードする
+        byte[] screenshot = tex.EncodeToPNG();
+
+#if UNITY_EDITOR
+        string savePath = Application.dataPath + "/../screenshot.png";
+#else
+        string savePath = Application.temporaryCachePath + "/screenshot-temp.png";
+#endif
+        Debug.LogFormat("Will store screenshot to '{0}'", savePath);
+
+        // FileStreamを開いてバイト配列をファイルに書き込む
+        using (FileStream fs = new FileStream(savePath, FileMode.Create, FileAccess.Write))
+        {
+            using (BinaryWriter bw = new BinaryWriter(fs))
+            {
+                bw.Write(screenshot);
+                bw.Close();
+            }
+            fs.Close();
+        }
+
+        Destroy(tex);
+    }
+}
